989dc24ad9f20213eaf4846c7abe691066e772b8
MenendezJuan <jumenendez01@gmail.com>
2025-08-03 21:57:03 -0300

    Add frmReporteInteligente form and move frmDespachoProducto
    Added new frmReporteInteligente form to Reportes module. Moved frmDespachoProducto files from Ventas to Stock, updating project references accordingly for better organization.

---
+++ Form1/Negocio/Reportes/frmReporteInteligente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheeseLogix.Negocio.Reportes
+{
+    public partial class frmReporteInteligente : Form
+    {
+        public frmReporteInteligente()
+        {
+            InitializeComponent();
+        }
+    }
+}
